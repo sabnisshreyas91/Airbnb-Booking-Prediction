8e1440d87632ad5bd9d9fd9a29b2a602ff76f187
Shreyas Sabnis <sabnisshreyas91@gmail.com>
2019-05-11 16:45:28 -0500

    moved config to src folder

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from os import path
+DEBUG = True
+LOGGING_CONFIG = "config/logging/local.conf"
+PORT = 3000
+APP_NAME = "penny-lane"
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/tracks.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+HOST = "127.0.0.1"
+# Getting the parent directory of this file. That will function as the project home.
+PROJECT_HOME = path.dirname(path.dirname(path.abspath(__file__)))
