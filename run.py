cdc233fac14a76060c641d178f91cc51dd4520f7
Shreyas Sabnis <sabnisshreyas91@gmail.com>
2019-05-22 00:50:21 -0500

    1) renamed src/helpers.py to data_ingest_schema_create.py 2) added doc string for 'create_schema' function in data_ingest_schema_create.py 3) Added slightly more obvious log messages in data_ingest_schema_create.py 4) Edited run to call data_ingest_schema_create.py instead of helpers.py and added optional arguments for bucket_name and bucket_folder so they can be fed as arguments rather than as configs. Configs now serve as default values

--- run.py
+++ run.py
@@ -10,11 +10,12 @@ a configurable target S3 bucket.
 import logging.config
 import src.config as config
 import os
+import argparse
 
 logging.config.fileConfig(config.LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
-from src.helper import download_source_zip, unzip_file, load_data_to_S3, create_schema
+from src.data_ingest_schema_create import download_source_zip, unzip_file, load_data_to_S3, create_schema
 
 root = config.PROJECT_HOME
 data = config.DATA_FOLDER
@@ -25,6 +26,11 @@ uncompressed_data = config.UNCOMPRESSED_DATA
 zip_file_name = config.ZIP_FILE_NAME
 bucket_name = config.BUCKET_NAME
 bucket_folder = config.BUCKET_FOLDER
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bucket_name", default= bucket_name, help="S3 bucket to upload the source data to. Default:nw-shreyassabnis-msia423")
+parser.add_argument("--bucket_folder", default= bucket_folder, help="Folder within S3 bucket where wd'd like the data to be uploaded. Default:Input/")
+args = parser.parse_args()
 
 user = os.environ.get("MYSQL_USER")
 password = os.environ.get("MYSQL_PASSWORD")
@@ -48,5 +54,5 @@ uncompressed_folder_path = data_folder_path+uncompressed_data
 if __name__ == '__main__':
     download_source_zip(src_bucket_name, zip_file_name, destination_path)
     unzip_file(uncompressed_folder_path, zip_file_name, zip_file_path )
-    load_data_to_S3(uncompressed_folder_path, bucket_name, bucket_folder)
+    load_data_to_S3(uncompressed_folder_path, args.bucket_name, args.bucket_folder)
     create_schema(user, password, host, port, databasename, sqlite_uri, rds_flag)
